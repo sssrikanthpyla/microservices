b025c43b828d323307a43b4c27ff564384596b69
sssrikanthpyla <41078258+sssrikanthpyla@users.noreply.github.com>
2020-05-01 13:51:28 +0530

    currency-exchange-service first commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into exchange_value (id,currency_from,currency_to,conversion_multiple,port) values (10001,'USD','INR',65,0);
+insert into exchange_value (id,currency_from,currency_to,conversion_multiple,port) values (10001,'EUR','INR',75,0);
+insert into exchange_value (id,currency_from,currency_to,conversion_multiple,port) values (10001,'AUD','INR',55,0);
